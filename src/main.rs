6c0523f76f077233a31a248676c9160752caafa7
Leandro Marceddu <leandromarceddu@gmail.com>
2022-03-10 17:25:47 +0100

    Processes are scary

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use std::io::{BufRead, BufReader};
-use subprocess::Exec;
+mod config;
+mod processes;
+
 use std::process;
 use config::Config;
-mod config;
-
-
 
 fn main() {
     if config::init::check_present() {
         println!("Config found.");
         let config: Config = config::init::read_config();
-        println!("{}", config.superslicer.path);
+        println!("{:#?}", config.superslicer.path);
         
     } else {
         println!("No config found, creating one.");
@@ -19,14 +17,4 @@ fn main() {
         println!("the config.toml currently created in the directory of which this app is located. Fire me up when ready! ;)");
         process::exit(exitcode::OK);
     }
-    
-    /*let x = Exec::cmd("C:\\Users\\leand\\Desktop\\SuperSlicer_2.3.57.11_win64_220213\\superslicer_console.exe")
-            .arg("-g")
-            .arg("C:\\Users\\leand\\Downloads\\connector_trim_jig.stl")
-            .stream_stdout()
-            .unwrap();
-    let br = BufReader::new(x);
-    for (i, line) in br.lines().enumerate() {
-        println!("{}: {}", i, line.unwrap());
-    }*/
 }
---
+++ src/processes.rs
@@ -0,0 +1,15 @@
+use subprocess::Exec;
+
+
+
+
+    
+    /*let x = Exec::cmd("C:\\Users\\leand\\Desktop\\SuperSlicer_2.3.57.11_win64_220213\\superslicer_console.exe")
+            .arg("-g")
+            .arg("C:\\Users\\leand\\Downloads\\connector_trim_jig.stl")
+            .stream_stdout()
+            .unwrap();
+    let br = BufReader::new(x);
+    for (i, line) in br.lines().enumerate() {
+        println!("{}: {}", i, line.unwrap());
+    }*/
