88b9084735c5b433cea93393df2bd000d5b5b1d7
Leandro Marceddu <leandromarceddu@gmail.com>
2022-03-10 17:01:44 +0100

    Close program to force the user to reopen, I'll figure something else out eventually

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{BufRead, BufReader};
 use subprocess::Exec;
+use std::process;
 use config::Config;
 mod config;
 
@@ -9,10 +10,14 @@ fn main() {
     if config::init::check_present() {
         println!("Config found.");
         let config: Config = config::init::read_config();
-        println!("{}", config.superslicer.path)
+        println!("{}", config.superslicer.path);
+        
     } else {
         println!("No config found, creating one.");
         config::init::create_config();
+        println!("Due to current development, we're closing this app for the time being so you can edit");
+        println!("the config.toml currently created in the directory of which this app is located. Fire me up when ready! ;)");
+        process::exit(exitcode::OK);
     }
     
     /*let x = Exec::cmd("C:\\Users\\leand\\Desktop\\SuperSlicer_2.3.57.11_win64_220213\\superslicer_console.exe")
