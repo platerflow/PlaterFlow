1c46773e8f03d3d533ea70ad2400c8e1e9d9583e
Leandro Marceddu <leandromarceddu@gmail.com>
2022-03-16 17:11:40 +0100

    Add ureq dep so I don't forget about it

--- src/processes.rs
+++ src/processes.rs
@@ -160,14 +160,3 @@ pub mod superslicer {
                 .unwrap();
     }
 }
-
-    
-    /*let x = Exec::cmd("C:\\Users\\leand\\Desktop\\SuperSlicer_2.3.57.11_win64_220213\\superslicer_console.exe")
-            .arg("-g")
-            .arg("C:\\Users\\leand\\Downloads\\connector_trim_jig.stl")
-            .stream_stdout()
-            .unwrap();
-    let br = BufReader::new(x);
-    for (i, line) in br.lines().enumerate() {
-        println!("{}: {}", i, line.unwrap());
-    }*/
